f042ff25b25693b6b725206275e3f144d8f14a24
hp2256 <harsh2256@gmail.com>
2021-09-08 15:34:36 +0530

    split databases

--- guests-mgmt-service/src/test/java/com/hp/guestsmgmtservice/GuestsMgmtServiceApplicationTests.java
+++ guests-mgmt-service/src/test/java/com/hp/guestsmgmtservice/GuestsMgmtServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.hp.guestsmgmtservice;
 
+import com.hp.guestsmgmtservice.models.Address;
+import com.hp.guestsmgmtservice.models.AllGuests;
+import com.hp.guestsmgmtservice.models.Guest;
+import com.hp.guestsmgmtservice.repos.GuestRepository;
+import com.hp.guestsmgmtservice.services.GuestsService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class GuestsMgmtServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    private GuestsService guestsService;
+    @Mock
+    private GuestRepository guestRepository;
 
+    @Test
+    void testAddGuest() {
+        Guest guest = new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India"));
+        when(guestsService.addGuest(guest)).thenReturn(guest);
+        assertEquals(guest, guestRepository.save(guest));
+    }
+    @Test
+    void testAll() {
+        Guest guest = new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India"));
+        Guest guest2 = new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India"));
+        List<Guest> allGuests= new ArrayList<>(Arrays.asList(guest,guest2));
+        when(guestsService.getAll()).thenReturn(allGuests);
+        assertEquals(2, guestsService.getAll().size());
+    }
+
+    @Test
+    void testFindOne() {
+
+//        Optional<Guest> guest = Optional.of(new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India")));
+//        Guest guest1 = new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India"));
+//
+//        guest.get().setId("1");
+//       Mockito. when(guestsService.getGuest("1")).thenReturn(guest);
+//        assertEquals(guest.get(), guestsService.getGuest("1"));
+    }
+
+    @Test
+    void testUpdateOne() {
+        Guest guest = new Guest(1, 122222222, "CG", "HP", "harsh@gmail.com", "male", new Address("street", 400000, "Mumb", "India"));
+        when(guestsService.updateGuest(guest)).thenReturn(guest);
+        assertEquals(guest,guestsService.updateGuest(guest));
+    }
 }
---
+++ room-mgmt-service/src/test/java/com/hp/roomsmgmtservice/RoomsMgmtServiceApplicationTests.java
@@ -0,0 +1,29 @@
+package com.hp.roomsmgmtservice;
+
+import com.hp.roomsmgmtservice.models.Rooms;
+import com.hp.roomsmgmtservice.repos.RoomsRepository;
+import com.hp.roomsmgmtservice.services.RoomsMgmtService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RoomsMgmtServiceApplicationTests {
+
+/*	@Mock
+	private RoomsRepository roomsRepository;
+	@InjectMocks
+	private RoomsMgmtService roomsMgmtService;
+	@Test
+	void testAddRoom() {
+
+
+	}*/
+
+}
